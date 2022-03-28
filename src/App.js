3ebdb578c63d75b6034ac8e2f3e6579219e93504
rose <rosedaily1001@gmail.com>
2022-03-29 06:03:19 +0900

    이벤트, state 진행

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+function Article(props){
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+  </article>
+}
 function Header(props){
   return <header>
-  <h1><a href="/">{props.title}</a></h1>
+  <h1><a href="/" onClick={(event)=>{
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
 </header>
 }
 function Nav(props){
@@ -10,7 +21,12 @@ function Nav(props){
   const lis = [];
   for(let i = 0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+       event.preventDefault();
+       props.onChangeMode(Number(event.target.id)); 
+      }}>{t.title}</a>
+    </li>)
     // 자동생성 태드의 경우 고유값 key를 부여해서 리액트가 추적할 수 있어야 한다. 
   }
   return <nav>
@@ -19,25 +35,40 @@ function Nav(props){
     </ol>
   </nav>
 }
-
-function Article(props){
-  return <article>
-  <h2>{props.title}</h2>
-  {props.body}
-  </article>
-}
-
 function App() {
+  //WELCOME 'READ';
+  const[mode, setMode] = useState('WELCOME');
+  const[id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'js', body:'js is...'}
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
